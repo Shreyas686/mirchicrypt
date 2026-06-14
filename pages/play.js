83474b58a91b3fbe9b2af38ac0b53da59b9c70ac
Shreyas686 <64825534+Shreyas686@users.noreply.github.com>
2026-06-14 12:21:02 +0530

    Update play.js

--- pages/play.js
+++ pages/play.js
@@ -1,4 +1,4 @@
-import { useSession } from "next-auth/client";
+import { useSession } from "next-auth/react";
 import { useState, useEffect } from "react";
 import LoadingFullScreen from "../components/helping";
 import axios from "axios";
