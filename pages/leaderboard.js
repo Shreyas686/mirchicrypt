0b10b5af7dd935043ad33d8686878e237e8c7806
Shreyas686 <64825534+Shreyas686@users.noreply.github.com>
2026-06-14 12:20:27 +0530

    Update leaderboard.js

--- pages/leaderboard.js
+++ pages/leaderboard.js
@@ -1,5 +1,5 @@
 import ErrorScreen from "../components/error";
-import { useSession } from "next-auth/client";
+import { useSession } from "next-auth/react";
 import { useState, useEffect } from "react";
 import LoadingFullScreen from "../components/helping";
 import axios from "axios";
